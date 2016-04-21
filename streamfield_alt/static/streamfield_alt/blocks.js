fa7fa0101f6ded678c2afa1a3536d1bdd569354b
Karl Hobley <karlhobley10@gmail.com>
2016-04-21 22:22:40 +0100

    Added logic for telling when to show reorder buttons

--- streamfield_alt/static/streamfield_alt/blocks.js
+++ streamfield_alt/static/streamfield_alt/blocks.js
@@ -61,13 +61,24 @@ class StreamMenu extends React.Component {
 
 class StreamChild extends React.Component {
     render() {
+        let actionButtons = [];
+
+        if (!this.props.isFirst) {
+            actionButtons.push(<button key="moveup" type="button" id={`${this.props.path}-moveup`} title="Move up" className="icon text-replace icon-order-up">Move up</button>);
+        }
+
+        if (!this.props.isLast) {
+            actionButtons.push(<button key='movedown' type="button" id={`${this.props.path}-movedown`} title="Move down" className="icon text-replace icon-order-down">Move down</button>);
+        }
+
+        actionButtons.push(<button key='delete' type="button" id={`${this.props.path}-delete`} title="Delete" className="icon text-replace hover-no icon-bin">Delete</button>);
+
+
         return <li id={`${this.props.path}-container`} className={`sequence-member blockname-${this.props.type}`}>
             <div className="sequence-controls">
                 <h3><label for={`${this.props.path}-value`}>{this.props.schema.label}</label></h3>
                 <div className="button-group button-group-square">
-                    <button type="button" id={`${this.props.path}-moveup`} title="Move up" className="icon text-replace icon-order-up disabled">Move up</button>
-                    <button type="button" id={`${this.props.path}-movedown`} title="Move down" className="icon text-replace icon-order-down">Move down</button>
-                    <button type="button" id={`${this.props.path}-delete`} title="Delete" className="icon text-replace hover-no icon-bin">Delete</button>
+                    {actionButtons}
                 </div>
             </div>
             <div className="sequence-member-inner ">
@@ -87,8 +98,10 @@ class StreamBlock extends React.Component {
             let type = this.props.value[id].type;
             let value = this.props.value[id].value;
             let schema = this.props.schema.child_blocks[type];
+            let isFirst = id == 0;
+            let isLast = id ==this.props.value.length - 1;
 
-            childBlocks.push(<StreamChild key={id} path={path} type={type} value={value} schema={schema} parentSchema={this.props.schema} />);
+            childBlocks.push(<StreamChild key={id} path={path} type={type} value={value} schema={schema} parentSchema={this.props.schema} isFirst={isFirst} isLast={isLast} />);
         }
 
         return <div className="field block_field block_widget ">
